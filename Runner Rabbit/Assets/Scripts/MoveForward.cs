c1f81af0ba020f70604c66cf5d65299a79433037
Juan Felipe Medrano <>
2020-01-18 17:21:20 -0500

    Buenas, Push del proyecto.

---
+++ Runner Rabbit/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    private float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}
---
+++ Runner Rabbit/Assets/Scripts/character.cs
@@ -0,0 +1,101 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private SpriteRenderer mySpriteRenderer;
+
+
+    public float upspeed;
+    
+    private float speed = 10f;
+
+    public Animator animator;
+
+    public bool top;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        rb.AddForce(new Vector2(0, 200));
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+
+    {
+
+      
+
+        // Jump Controller
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, upspeed));
+
+        }
+
+            // Animation Controller
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetBool("IsFalling", true);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                animator.SetBool("IsFalling", false);
+            }
+
+
+
+        
+
+
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime;
+            transform.position = temp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("rift entered");
+        Rotation();
+        rb.gravityScale *= -1;
+        upspeed *= -1;
+        rb.AddForce(new Vector2(0, (upspeed*10)));
+
+    }
+
+    void Rotation()
+    {
+
+        {
+            //transform.eulerAngles = new Vector3(0, 0, 180f);
+
+
+            top = !top; // toggles onoff at each click
+
+            if (top)
+            {
+                print("left");
+                mySpriteRenderer.flipY = true;
+            }
+            else
+            {
+                print("right");
+                mySpriteRenderer.flipY = false;
+            }
+
+        }   
+    }
+
+}
