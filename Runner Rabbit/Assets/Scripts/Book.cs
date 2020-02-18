63ed672b206755b3ea4a914c562c17197deb6034
Rafael Ospina <rafaelospiname@gmail.com>
2020-02-18 17:21:19 -0500

    Enemy Spawn

---
+++ Runner Rabbit/Assets/Scripts/Book.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    Animator anim;
+    public int attackNumber;
+    public int attackMax;
+    EnemySpawner enemySpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        attackMax = Random.Range(1, 4);
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackNumber >= attackMax)
+        {
+            anim.SetTrigger("Die");
+        }
+    }
+    void AddAttack()
+    {
+        attackNumber = attackNumber + 1;
+    }
+    void Over()
+    {
+        enemySpawner.RestartTime();
+        Destroy(gameObject);
+    }
+    
+}
---
+++ Runner Rabbit/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float spawnTime;
+    public bool enemy;
+    int enemyType = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTime = Random.Range(3f, 6f);
+        //enemyType = Random.Range(1, 5); ToDo
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTime -= Time.deltaTime;
+        if (spawnTime <= 0 && !enemy)
+        {
+            if (enemyType == 1)
+            {
+                GameObject enemy = GameObject.Instantiate(Resources.Load("Prefabs/Firebook") as GameObject);
+                enemy.transform.position = transform.position;
+            }
+            /*if (enemyType == 2) ToDo
+            {
+                GameObject enemy = GameObject.Instantiate(Resources.Load("Prefabs/OtroEnemigo") as GameObject);
+                enemy.transform.position = transform.position;
+            }*/
+            enemy = true;
+        }
+    }
+
+    public void RestartTime()
+    {
+        spawnTime = Random.Range(3f, 6f);
+        enemy = false;
+        //enemyType = Random.Range(1, 5); ToDo
+    }
+}
