8d7c5dc3fee3a78c9d2862882dff1e55cad74489
Rafael Ospina <rafaelospiname@gmail.com>
2020-03-06 17:56:16 -0500

    Vidas enemigos

--- Runner Rabbit/Assets/Scripts/Slime.cs
+++ Runner Rabbit/Assets/Scripts/Slime.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slime : MonoBehaviour
 {
     Animator anim;
-    public int health = 3;
+    public int maxHealth = 3;
+    public int health;
     EnemySpawner enemySpawner;
+    public Slider healthSlider;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         enemySpawner = FindObjectOfType<EnemySpawner>();
+        healthSlider = GetComponentInChildren<Slider>();
+        health = maxHealth;
+        healthSlider.maxValue = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        healthSlider.value = health;
         if (health <= 0)
         {
             anim.SetTrigger("Die");
