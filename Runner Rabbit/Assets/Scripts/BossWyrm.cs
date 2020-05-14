3820d8ade4775827192a21e77145e572b2666bbd
Rafael Ospina <rafaelospiname@gmail.com>
2020-05-14 17:26:01 -0500

    Attacks are triggered by keys. Will be set to random later.

--- Runner Rabbit/Assets/Scripts/BossWyrm.cs
+++ Runner Rabbit/Assets/Scripts/BossWyrm.cs
@@ -7,6 +7,7 @@ public class BossWyrm : MonoBehaviour
     public int element = 1;
     public int currentElement = 1;
     public GameObject[] portals;
+    public GameObject[] attacks;
     public bool ice;
     public float iceTimer = 10f;
     Animator anim;
@@ -47,11 +48,26 @@ public class BossWyrm : MonoBehaviour
             ice = false;
             iceTimer = 10f;
         }
-        if (Input.GetKeyDown(KeyCode.A)) //for testing
+        /*if (Input.GetKeyDown(KeyCode.U)) //for testing
         {
             anim.SetTrigger("Despawn"); //this will trigger the portal change
             
         }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (element == 1)
+            {
+                anim.SetTrigger("Shoot");
+            }
+            if (element == 2)
+            {
+                anim.SetTrigger("Shoot");
+            }
+            if(element == 3)
+            {
+                anim.SetTrigger("Shoot");
+            }
+        }*/
         
     }
     public void ChangeElement()
@@ -67,5 +83,40 @@ public class BossWyrm : MonoBehaviour
     public void NewPortal()
     {
         portals[currentElement - 1].GetComponent<Animator>().SetTrigger("Change");
+    }
+    public void Shoot()
+    {
+        if (element == 1)
+        {
+            GameObject fireBall = Instantiate(Resources.Load("Prefabs/WyrmFireBall") as GameObject);
+            fireBall.transform.position = transform.position + new Vector3(-5.755793f, -0.2495456f, 0);
+        }
+        if (element == 3)
+        {
+            GameObject thunderBall = Instantiate(Resources.Load("Prefabs/WyrmThunderBall") as GameObject);
+            thunderBall.transform.position = transform.position + new Vector3(-5.755793f, -0.2495456f, 0);
+        }
+    }
+    public void RiftAttack()
+    {
+        if (element == 2)
+        {
+            attacks[0].SetActive(true);
+        }
+        if (element == 3)
+        {
+            attacks[3].SetActive(true);
+        }
+    }
+    public void Ray()
+    {
+        if(element == 1)
+        {
+            attacks[1].SetActive(true);
+        }
+        if (element == 2)
+        {
+            attacks[2].SetActive(true);
+        }
     }
 }
