678d5b908db08ea17702c65bfdb42f0e2e16b41e
Juan Felipe Medrano <>
2020-05-15 13:35:01 -0500

    More Things for the menu

---
+++ Runner Rabbit/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Animator transition;
+
+    public void LoadLevel( int SceneIndex)
+
+    {
+        StartCoroutine(loadAsync(SceneIndex));
+    }
+
+    IEnumerator loadAsync (int SceneIndex)
+
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            Debug.Log(progress);
+
+            yield return null;
+
+
+        }
+    }
+
+
+}
