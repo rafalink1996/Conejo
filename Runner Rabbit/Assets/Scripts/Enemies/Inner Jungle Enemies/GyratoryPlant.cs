3605ffdeeecbff92b2c5475243665f97c8ee6d6a
Juan Felipe Medrano <>
2020-07-21 14:37:22 -0500

    Enemies in inner jungle

---
+++ Runner Rabbit/Assets/Scripts/Enemies/Inner Jungle Enemies/GyratoryPlant.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyratoryPlant : MonoBehaviour
+{
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}
+    
+
---
+++ Runner Rabbit/Assets/Scripts/Enemies/Inner Jungle Enemies/ShooterPlant.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterPlant : MonoBehaviour
+{
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateTowards(target.position);
+    }
+
+    private void RotateTowards(Vector2 target)
+    {
+        var offset = 180f;
+        Vector2 direction = target - (Vector2)transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
+    }
+}
