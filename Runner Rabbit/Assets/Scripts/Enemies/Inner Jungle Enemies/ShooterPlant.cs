c9f215c033798142fb7eb4f99d71e8443cf93c3a
Juan Felipe Medrano <>
2020-07-22 13:51:40 -0500

    Jungle enemies work

--- Runner Rabbit/Assets/Scripts/Enemies/Inner Jungle Enemies/ShooterPlant.cs
+++ Runner Rabbit/Assets/Scripts/Enemies/Inner Jungle Enemies/ShooterPlant.cs
@@ -6,17 +6,68 @@ public class ShooterPlant : MonoBehaviour
 {
 
     private Transform target;
+    Animator anim;
+    public EnemySpawner enemySpawner;
+    float spawnTime;
+    bool spawned = false;
+    float attackTime;
+    bool attack;
+    EnemyHealth health;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindWithTag("Player").transform;
+        health = GetComponent<EnemyHealth>();
+        health.maxHealth = 50;
+        anim = GetComponent<Animator>();
+        if (transform.position.y > 0)
+        {
+            enemySpawner = GameObject.Find("Enemy Spawner (Up)").GetComponent<EnemySpawner>();
+        }
+        if (transform.position.y < 0)
+        {
+            enemySpawner = GameObject.Find("Enemy Spawner (Down)").GetComponent<EnemySpawner>();
+        }
+        //enemySpawner = FindObjectOfType<EnemySpawner>();
+        //healthSlider = GetComponentInChildren<Slider>();
+        //health = maxHealth;
+        //healthSlider.maxValue = maxHealth;
+        spawnTime = Random.Range(0.1f, 2f);
+        attackTime = Random.Range(0.2f, 1.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
         RotateTowards(target.position);
+
+        spawnTime -= Time.deltaTime;
+        if (spawnTime <= 0 && spawned == false)
+        {
+            anim.SetTrigger("Spawn");
+            GetComponent<SpriteRenderer>().enabled = true;
+            FindObjectOfType<AudioManager>().Play("BookSpawn");
+            spawned = true;
+
+        }
+        if (attack)
+        {
+            attackTime -= Time.deltaTime;
+        }
+        if (attackTime <= 0)
+        {
+            anim.SetTrigger("Attack");
+            attackTime = Random.Range(0.2f, 1.3f);
+            attack = false;
+        }
+
+        //healthSlider.value = health;
+        if (health.health <= 0)
+        {
+            anim.SetTrigger("Die");
+
+        }
     }
 
     private void RotateTowards(Vector2 target)
@@ -26,5 +77,38 @@ public class ShooterPlant : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
+    }
+    void AttackTime()
+    {
+        if (!attack)
+        {
+            attack = true;
+        }
+    }
+    void Attack()
+    {
+        health.TakeDamage(10);
+    }
+    void Peashooter()
+    {
+        GameObject peashooter = GameObject.Instantiate(Resources.Load("Prefabs/Peashooter") as GameObject);
+        peashooter.transform.position = transform.position;
+
+    }
+    void Over()
+    {
+        enemySpawner.OneDown();
+        Destroy(gameObject);
+        if (health.Hit == true)
+        {
+            GameObject healthHeal = GameObject.Instantiate(Resources.Load("prefabs/HeartHeal") as GameObject);
+            healthHeal.transform.position = transform.position;
+        }
+    }
+
+    void Despawned()
+    {
+        FindObjectOfType<AudioManager>().Play("BookDeSpawn");
+
     }
 }
