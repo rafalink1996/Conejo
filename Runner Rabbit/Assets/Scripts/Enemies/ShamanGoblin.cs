813297472633bde85df285950c2ec21239574617
Rafael Ospina <rafaelospiname@gmail.com>
2020-08-24 17:14:33 -0500

    Messing with shaman

---
+++ Runner Rabbit/Assets/Scripts/Enemies/ShamanGoblin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShamanGoblin : MonoBehaviour
+{
+    public float spawnTime;
+    public bool isPresent;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTime = Random.Range(0.5f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTime -= Time.deltaTime;
+        if (spawnTime <= 0)
+        {
+            spawnTime = 30f;
+            if (!isPresent)
+            {
+                anim.SetTrigger("Spawn");
+                isPresent = true;
+            }
+        }
+    }
+}
