0e8be0c11855bfc35368cf6b487465020c3e2acc
Juan Felipe Medrano <>
2020-09-16 13:34:53 -0500

    starting with skin system

---
+++ Runner Rabbit/Assets/Scripts/SkinSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinSystem : MonoBehaviour
+{
+    public AnimatorOverrideController[] Skins;
+    public character character;
+
+
+
+    public int TopHatSkinIDTop;
+    public int TopHatSkinIDBot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     if (character.top)
+        {
+            GetComponent<Animator>().runtimeAnimatorController = Skins[TopHatSkinIDTop] as RuntimeAnimatorController;
+        } else
+        {
+            GetComponent<Animator>().runtimeAnimatorController = Skins[TopHatSkinIDBot] as RuntimeAnimatorController;
+        }
+            
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (character.top)
+        {
+            GetComponent<Animator>().runtimeAnimatorController = Skins[TopHatSkinIDTop] as RuntimeAnimatorController;
+        }
+        else
+        {
+            GetComponent<Animator>().runtimeAnimatorController = Skins[TopHatSkinIDBot] as RuntimeAnimatorController;
+        }
+
+    }
+}
