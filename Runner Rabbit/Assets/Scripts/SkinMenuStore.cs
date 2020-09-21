951a36643ed632ee77808a04e183e00415f412a3
Juan Felipe Medrano <>
2020-09-21 14:59:21 -0500

    Moar skins and working on menu skin system

---
+++ Runner Rabbit/Assets/Scripts/SkinMenuStore.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinMenuStore : MonoBehaviour
+{
+    public Image SkinTopPreview;
+    public Image SkinBotPreview;
+    public Image selectdisplayimage;
+
+
+    public Sprite[] SkinIcons;
+
+    public int SelectedSkinID;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectdisplayimage.sprite = SkinIcons[SelectedSkinID];
+
+        SkinTopPreview.sprite = SkinIcons[GameStats.stats.topSkinID];
+        SkinBotPreview.sprite = SkinIcons[GameStats.stats.botSkinID];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selectdisplayimage.sprite = SkinIcons[SelectedSkinID];
+
+        SkinTopPreview.sprite = SkinIcons[GameStats.stats.topSkinID];
+        SkinBotPreview.sprite = SkinIcons[GameStats.stats.botSkinID];
+    }
+
+    public void SelectSkin (int SkinID)
+	{
+        SelectedSkinID = SkinID;
+	}
+
+
+    public void confirmSkinTop ()
+    {
+        GameStats.stats.topSkinID = SelectedSkinID;
+
+    }
+
+    public void confirmSkinBot()
+    {
+        GameStats.stats.botSkinID = SelectedSkinID;
+
+    }
+
+
+}
