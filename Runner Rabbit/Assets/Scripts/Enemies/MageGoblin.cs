b336b504d2cf39afda9f7c46c59b0b20394ac37f
Rafael Ospina <rafaelospiname@gmail.com>
2020-09-22 17:23:36 -0500

    El mago ya puede atacar e invocar a los dos trolls por separado. Si un troll muere, puede volver a invocar solo al que murió.

--- Runner Rabbit/Assets/Scripts/Enemies/MageGoblin.cs
+++ Runner Rabbit/Assets/Scripts/Enemies/MageGoblin.cs
@@ -9,13 +9,13 @@ public class MageGoblin : MonoBehaviour
     bool attack;
     bool invoke;
     Animator anim;
-    public GameObject troll1;
-    public GameObject troll2;
+    public GameObject troll1Object;
+    public GameObject troll2Object;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        attackTime = Random.Range(3f, 8f);
+        attackTime = Random.Range(3f, 4f);
         invokeTime = Random.Range(8f, 15f);
 
     }
@@ -23,7 +23,7 @@ public class MageGoblin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (troll1 == null || troll2 == null)
+        if (troll1Object == null || troll2Object == null)
         {
             invokeTime -= Time.deltaTime;
         }
@@ -43,6 +43,7 @@ public class MageGoblin : MonoBehaviour
             else
             {
                 anim.SetTrigger("Invoke");
+                attack = true;
             }
         }
     }
@@ -51,11 +52,26 @@ public class MageGoblin : MonoBehaviour
         GameObject fireball = Instantiate(Resources.Load("Prefabs/WyrmFireBall") as GameObject);
         fireball.GetComponentInChildren<WyrmFireBall>().sourceTransform = gameObject.transform;
         fireball.transform.position = transform.position + new Vector3(-5.5f, -0.1f, 0);
-        attackTime = Random.Range(3f, 8f);
+        attackTime = Random.Range(3f, 5f);
         attack = false;
     }
     void Invoke()
     {
+        if (troll1Object == null)
+        {
+            GameObject troll1 = Instantiate(Resources.Load("Prefabs/TrollGoblin") as GameObject);
+            troll1Object = troll1;
+            troll1.transform.position = new Vector3(transform.position.x - 6.2f, 4.31f, 0);
+        }
+        if (troll2Object == null)
+        {
+            GameObject troll2 = Instantiate(Resources.Load("Prefabs/TrollGoblin") as GameObject);
+            troll2Object = troll2;
+            troll2.transform.position = new Vector3(transform.position.x - 6.2f, -4.31f, 0);
+        }
+        attackTime = Random.Range(3f, 5f);
+        attack = false;
+        invokeTime = Random.Range(8f, 15f);
         invoke = false;
     }
 }
