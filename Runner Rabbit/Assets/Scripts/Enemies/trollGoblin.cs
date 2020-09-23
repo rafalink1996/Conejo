cbb125556cf6f19233e844aa0bb7e6beab11cc3c
Rafael Ospina <rafaelospiname@gmail.com>
2020-09-23 17:09:23 -0500

    Ya casi el boss

---
+++ Runner Rabbit/Assets/Scripts/Enemies/trollGoblin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trollGoblin : MonoBehaviour
+{
+    public float attackTime;
+    bool attack;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackTime = Random.Range(1.3f, 4f);
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!attack)
+        {
+            attackTime -= Time.deltaTime;
+        }
+        if (attackTime <= 0 && !attack)
+        {
+            anim.SetFloat("AttackType", Random.Range(1, 3));
+            anim.SetTrigger("Attack");
+            attack = true;
+        }
+    }
+    void Attack()
+    {
+        //instantiate thingy
+        attackTime = Random.Range(1.3f, 4f);
+        attack = false;
+    }
+}
