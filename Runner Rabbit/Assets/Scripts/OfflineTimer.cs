3c4057824fa938bb832e79133506f109c463ad1f
Juan Felipe Medrano <35904953+Acidoswaldo@users.noreply.github.com>
2020-10-14 13:53:15 -0500

    tratando de crear un timer que siga funcionando cuando la app se cierre para los ads de la tienda;

---
+++ Runner Rabbit/Assets/Scripts/OfflineTimer.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using TMPro;
+
+public class OfflineTimer : MonoBehaviour
+{
+    public TextMeshProUGUI counterText, pauseText, resumeText, msgText;
+
+    private int counterValue, focusCounter, pauseCounter;
+    private DateTime lastMinimize;
+    private double minimizedSeconds;
+
+    void OnApplicationPause(bool isGamePause)
+    {
+        if (isGamePause)
+        {
+            pauseCounter++;
+            pauseText.text = "Paused : " + pauseCounter;
+            GoToMinimize();
+
+            Debug.Log("minimized");
+        }
+    }
+
+    void OnApplicationFocus(bool isGameFocus)
+    {
+        if (isGameFocus)
+        {
+            focusCounter++;
+            resumeText.text = "Focused : " + focusCounter;
+            GoToMaximize();
+
+            Debug.Log("maximize");
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("StartCounter");
+        Application.runInBackground = true;
+    }
+
+    IEnumerator StartCounter()
+    {
+        yield return new WaitForSeconds(1f);
+        counterText.text = "Counter : " + counterValue.ToString();
+        counterValue++;
+        StartCoroutine("StartCounter");
+    }
+
+    public void GoToMinimize()
+    {
+        lastMinimize = DateTime.Now;
+    }
+
+    public void GoToMaximize()
+    {
+        if (focusCounter >= 2)
+        {
+            minimizedSeconds = (DateTime.Now - lastMinimize).TotalSeconds;
+            msgText.text = "Total Minimized Seconds : " + ((Int32)minimizedSeconds).ToString();
+            counterValue += (Int32)minimizedSeconds;
+        }
+
+    }
+
+}
