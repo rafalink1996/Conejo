c493145e2e2cdbd4ec224e30f03f497d7c0b61e2
Rafael Ospina <rafaelospiname@gmail.com>
2020-11-11 15:03:14 -0500

    Sigo con el boss

--- Runner Rabbit/Assets/Scripts/Enemies/ICE enemies/IceLances.cs
+++ Runner Rabbit/Assets/Scripts/Enemies/ICE enemies/IceLances.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class IceLances : MonoBehaviour
 {
+    public Transform[] lance;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class IceLances : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        lance[0].position = Vector2.MoveTowards(lance[0].transform.position, new Vector2(lance[0].transform.position.x, 10), 2 * Time.deltaTime);
+        lance[1].position = Vector2.MoveTowards(lance[1].transform.position, new Vector2(lance[1].transform.position.x, 8), 2 * Time.deltaTime);
+        lance[2].position = Vector2.MoveTowards(lance[2].transform.position, new Vector2(lance[2].transform.position.x, 6), 2 * Time.deltaTime);
+        lance[3].position = Vector2.MoveTowards(lance[3].transform.position, new Vector2(lance[3].transform.position.x, 4), 2 * Time.deltaTime);
     }
 }
