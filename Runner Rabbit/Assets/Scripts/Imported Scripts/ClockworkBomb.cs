3997c5e66fef90800d8e1601ec4ccfac8cbffbf6
Juan Felipe Medrano <35904953+Acidoswaldo@users.noreply.github.com>
2020-11-19 13:34:09 -0500

    arreglando enemigos nivel 5 (parte 1)

---
+++ Runner Rabbit/Assets/Scripts/Imported Scripts/ClockworkBomb.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockworkBomb : MonoBehaviour
+{
+
+    
+    public float timeToExplode;
+    public float speed = 5f;
+    
+    public Animator animator;
+    EnemyHealth health;
+    
+    public CircleCollider2D ExplotionCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        health = GetComponent<EnemyHealth>();
+        health.maxHealth = 10;
+        StartCoroutine(ExplodeTimer());
+        ExplotionCollider.enabled = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
+
+        if (health.health <= 0 || GameStats.stats.spawnHouse)
+        {
+            animator.SetTrigger("Explode!");
+          
+
+        }
+
+    }
+
+    IEnumerator ExplodeTimer()
+    {
+        yield return new WaitForSeconds(timeToExplode);
+        ExplotionCollider.enabled = true;
+        animator.SetTrigger("Explode!");
+        if (ExplotionCollider.radius < 1.25f)
+        {
+            ExplotionCollider.radius += 0.2f;
+        }
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+
+
+    }
+
+    void Over()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+
+}
