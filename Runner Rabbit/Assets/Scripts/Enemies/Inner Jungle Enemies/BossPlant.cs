01479b34fee9fadef3b649d1b365b9d231a61b8c
Rafael Ospina <rafaelospiname@gmail.com>
2020-12-03 16:23:43 -0500

    Atacando con plantas

---
+++ Runner Rabbit/Assets/Scripts/Enemies/Inner Jungle Enemies/BossPlant.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPlant : MonoBehaviour
+{
+    public GameObject poisonAttack;
+    public GameObject leafAttack;
+    ParticleSystem poisonParticles;
+    Animator anim;
+    public float poisonTime;
+    bool poison = false;
+    
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        poisonParticles = poisonAttack.GetComponent<ParticleSystem>();
+        var main = poisonParticles.main;
+        main.duration = poisonTime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //if (!poisonParticles.IsAlive() && poison)
+        //{
+        //    Invoke("DeactivatePoison", poisonTime);
+        //}
+    }
+    void ActivatePosion()
+    {
+        poisonParticles.Play();
+        //poisonAttack.SetActive(true);
+        poison = true;
+        Invoke("DeactivatePoison", poisonTime);
+    }
+
+    void DeactivatePoison()
+    {
+        //poisonAttack.SetActive(false);
+        poisonParticles.Stop();
+        anim.SetTrigger("Attack1End");
+        //poisonTime = 4f;
+        poison = false;
+    }
+    void ActivateLeaves()
+    {
+        leafAttack.SetActive(true);
+        Invoke("DeactivateLeaves", 4f);
+    }
+    void DeactivateLeaves()
+    {
+        leafAttack.SetActive(false);
+    }
+}
