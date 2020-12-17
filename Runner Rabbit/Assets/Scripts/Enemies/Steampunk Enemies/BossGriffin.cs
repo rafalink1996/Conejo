96aeb41824c210a9c07f7921fb66e0720adf57ed
Rafael Ospina <rafaelospiname@gmail.com>
2020-12-17 16:26:49 -0500

    Empezando boss 5to nivel

---
+++ Runner Rabbit/Assets/Scripts/Enemies/Steampunk Enemies/BossGriffin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGriffin : MonoBehaviour
+{
+    public GameObject rayAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void RayAttack()
+    {
+        rayAttack.SetActive(true);
+    }
+}
