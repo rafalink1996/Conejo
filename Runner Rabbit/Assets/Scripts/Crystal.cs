52d8608b986ed31090b2d1df6cd5fe55dfadc861
Juan Felipe Medrano <35904953+Acidoswaldo@users.noreply.github.com>
2021-01-12 11:52:42 -0500

    Systema de Runas implementado! solo falta "destruction Rune"

--- Runner Rabbit/Assets/Scripts/Crystal.cs
+++ Runner Rabbit/Assets/Scripts/Crystal.cs
@@ -7,19 +7,44 @@ public class Crystal : MonoBehaviour
 
     public float speed = 5f;
     public int myTokenCount;
+    public Transform PlayerTarget;
 
     void Start()
     {
         FindObjectOfType<TokenSpawner>().SetTokenCount(myTokenCount);
         Destroy(gameObject, 10f);
+        PlayerTarget = GameObject.FindObjectOfType<character>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 temp = transform.position;
-        temp.x += speed * Time.deltaTime;
-        transform.position = temp;
+        if (GameStats.stats.Rune1 == GameStats.Rune.MagnetRune || GameStats.stats.Rune2 == GameStats.Rune.MagnetRune)
+        {
+            // rune is active
+            float dis = Vector3.Distance(PlayerTarget.position, transform.position);
+
+            if (dis < 5)
+            {
+                // player is within magnet range
+                float step = 0.5f;
+                transform.position = Vector3.MoveTowards(transform.position, PlayerTarget.position, step);
+            }
+            else
+            {
+                //player si not within magnet range
+                Vector3 temp = transform.position;
+                temp.x += speed * Time.deltaTime;
+                transform.position = temp;
+            }
+        }
+        else
+        {
+            // rune is not active
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime;
+            transform.position = temp;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
