b5c2b1eab226b55a0a74fd039c1bbae0c6b329d2
Rafael Ospina <rafaelospiname@gmail.com>
2021-02-03 17:38:01 -0500

    Debug texts

---
+++ Runner Rabbit/Assets/Scripts/DebugTexts.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugTexts : MonoBehaviour
+{
+    public Text lightMana;
+    public Text darkMana;
+    public Text levelCount;
+    public Text levelIndicator;
+    ManaHandle mana;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mana = FindObjectOfType<ManaHandle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mana != null)
+        {
+            lightMana.text = "Light Mana: " + mana.CurrentLightMana.ToString("F2");
+            darkMana.text = "Dark Mana: " + mana.CurrentDarkMana.ToString("F2");
+        }
+        levelCount.text = "Level Count: " + GameStats.stats.LevelCount;
+        levelIndicator.text = "Level Indicator: " + GameStats.stats.LevelIndicator;
+    }
+}
